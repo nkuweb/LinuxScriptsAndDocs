ead5fc9efd4f71b135e150dae239768202eb82ef
UtkucanBykl <utkucanbykl@gmail.com>
2017-04-11 01:34:00 +0300

    Deneme

--- fabric/fabfile.py
+++ fabric/fabfile.py
@@ -1,11 +1,17 @@
 
 from fabric.api import run
 from fabric.api import local
+from fabric.operations import sudo, prompt, put
+
+
 def host_type():
 	local ("uname -s")
 def make_file():
 	local ("mkdir hallo")
 def say_hello():
 	local("echo Hello")
+def sendFile():
+	path=raw_input("Path")
+	put(path,None,False,True)
 ##Write Tests Git Integration
 
