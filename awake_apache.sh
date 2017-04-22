25a98d22a017293901b09def8522e48cd4d7bd9f
Wood Programmer <emirozbir@hotmail.com>
2017-04-22 20:20:32 +0300

    Fixed

--- awake_apache.sh
+++ awake_apache.sh
@@ -1,8 +1,3 @@
-YOUR_INTERFACE = arg
 sudo apt-get update
 sudo apt-get install apache2
 sudo service apache2 restart
-i=0
-x=$(ifconfig $arg | grep inet | awk '{ print $2 }')
-
-echo "LOOk the second line above  write it your browser."
