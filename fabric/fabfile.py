a5083310d0846965e872ff4f15cd8807f8cd08e7
Wood Programmer <emirozbir@hotmail.com>
2017-04-22 23:11:19 +0300

    TESTED

--- fabric/fabfile.py
+++ fabric/fabfile.py
@@ -4,27 +4,32 @@ import os
 from fabric.api import *
 env.disable_known_hosts = True
 
-
 def run_main_script(file_name):
-	main_script = open(file_name)
-	run(main_script)
+		script_file = open('{0}'.format(file_name))
+		run(script_file.read())
+		script_file.close()
+
+
+def backup_host():
+	run_main_script("backup_host.sh")
 
 def awake_apache():
-	apache_scripts = open("awake_apache.sh")
-	run(apache_scripts)
+	run_main_script ("awake_apache.sh")
 
-def sendFile():
-	path=raw_input("Path")
-	put(path,None,False,True)
-
-def get_backup():
-	run_main_script("backup.sh")
-	run_main_script("add_cron.sh")
-
-def awake_db(db_name):
-	run_main_script("db_awake.sh")
 
 '''def awake_nginx():
 ##Write Tests Git Integration
 ##ADD_CRON scriptlerinin ne zaman konusulacagini tartisalim.
+def sendFile():
+	path=raw_input("Path")
+	put(path,None,False,True)
+
+
+def awake_db(db_name):
+	run_main_script("db_awake.sh")
+	TIME=`date +%b-%d-%y`
+	sudo tar czf /$TIME.tar.gz --exclude=/backup.tar.gz --exclude=/home --exclude=/media --exclude=/dev --exclude=/mnt --exclude=/proc --exclude=/sys --exclude=/tmp /
+	scp -r $TIME.tar.gz $1:$2
+	echo "Backup End!"
+
 '''
